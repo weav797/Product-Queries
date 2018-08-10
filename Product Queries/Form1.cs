c6154560b1587bc1e0544b42eb89900b78220f89
Orion Weaver <awesomeboy7865@gmail.com>
2018-08-09 22:33:01 -0500

    Add files via upload

---
+++ Product Queries/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Product_Queries
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void productBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.productBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.productDBDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'productDBDataSet.Product' table. You can move, or remove it, as needed.
+            this.productTableAdapter.Fill(this.productDBDataSet.Product);
+
+        }
+
+        private void sortByPriceButton_Click(object sender, EventArgs e)
+        {
+            this.productTableAdapter.FillByPrice(this.productDBDataSet.Product);
+        }
+
+        private void unitsGreater100Button_Click(object sender, EventArgs e)
+        {
+            this.productTableAdapter.FillByUnits(this.productDBDataSet.Product);
+        }
+
+        private void averagePriceButton_Click(object sender, EventArgs e)
+        {
+            //Declare a variable to hold the average price
+            decimal averagePrice;
+
+            //Get the average price
+            averagePrice = (decimal)this.productTableAdapter.AveragePrice();
+
+            //Display average price
+            MessageBox.Show("Average price of all items: " +
+                averagePrice.ToString("c"));
+        }
+    }
+}
